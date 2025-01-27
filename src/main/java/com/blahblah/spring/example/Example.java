c60d366f7ed74717e443005ada0da9fe05911acb
atul-mps <atul.dhadse@highwirepress.com>
2025-01-28 01:05:39 +0530

    register a bean based on conditions

--- src/main/java/com/blahblah/spring/example/Example.java
+++ src/main/java/com/blahblah/spring/example/Example.java
@@ -1,13 +1,39 @@
 package com.blahblah.spring.example;
 
+import java.util.function.Supplier;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Example {
 
 	public static void main(String[] args) {
-		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-		Vehicle vehicle = context.getBean(Vehicle.class);
-		System.out.println(vehicle.getName());
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+//		Vehicle vehicle = context.getBean(Vehicle.class);
+//		System.out.println(vehicle.getName());
+
+		// register a bean with a condition
+
+		Supplier<Vehicle> altoSupplier = () -> {
+			Vehicle alto = new Vehicle();
+			alto.setName("blah blah alto");
+			return alto;
+		};
+
+		Vehicle audi = new Vehicle();
+		audi.setName("blah blah audi");
+		Supplier<Vehicle> audiSupplier = () -> audi;
+
+		int num = 10;
+		if ((num & 1) == 0) {
+			context.registerBean("alto", Vehicle.class, altoSupplier);
+		} else {
+			context.registerBean("audi", Vehicle.class, audiSupplier);
+		}
+
+		var altoObj = context.getBean("alto", Vehicle.class);
+		System.out.println(altoObj.getName());
+
+		context.close();
 	}
 
 }
--- src/main/java/com/blahblah/spring/example/ProjectConfig.java
+++ src/main/java/com/blahblah/spring/example/ProjectConfig.java
@@ -1,21 +1,23 @@
 package com.blahblah.spring.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.blahblah.spring.example")
 public class ProjectConfig {
 
-	@Bean
-	Vehicle getVehicle() {
-		var veh = new Vehicle();
-		veh.setName("temp temp");
-		return veh;
-	}
-
-	@Bean
-	String getName() {
-		return "Spring";
-	}
+//	@Bean
+//	Vehicle getVehicle() {
+//		var veh = new Vehicle();
+//		veh.setName("temp temp");
+//		return veh;
+//	}
+//
+//	@Bean
+//	String getName() {
+//		return "Spring";
+//	}
 
 }
